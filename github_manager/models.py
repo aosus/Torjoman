fec7fdb2ef717c35ea91f08e9c8c76a7c5daee02
Karam <krmhmade1@gmail.com>
2022-09-22 09:14:02 +0300

    Add Moderator Model in github_manager

--- github_manager/models.py
+++ github_manager/models.py
@@ -5,3 +5,15 @@ from django.db import models
 class PullRequest(models.Model):
     prid = models.IntegerField()
     words = models.ManyToManyField("translation.Word", blank=True)
+
+class Moderator(models.Model):
+    username = models.CharField(max_length=255)
+    
+    @property
+    def get_all_moderators(self) -> list[str]:
+        """get_all_moderators Return a list of moderators usernames
+
+        Returns:
+            list[str]: The list of moderators usernames
+        """        
+        return [mod.username for mod in Moderator.objects.all()]
