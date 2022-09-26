98a4a940deafe795e445ed293a10fc5758403a13
Karam <krmhmade1@gmail.com>
2022-09-26 07:21:23 +0300

    Edit PR & Word relationship to ForeignKey

--- github_manager/models.py
+++ github_manager/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 class PullRequest(models.Model):
     prid = models.IntegerField()
-    words = models.ManyToManyField("translation.Word", blank=True)
+    words = models.ForeignKey("translation.Word")
 
 class Moderator(models.Model):
     username = models.CharField(max_length=255)
