fde545f15b2efdc6716d5aa126e504091c1f6419
Karam <krmhmade1@gmail.com>
2022-10-03 12:23:38 +0300

    Add Moderator to django admin

--- github_manager/admin.py
+++ github_manager/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import PullRequest
+from .models import Moderator, PullRequest
 
 # Register your models here.
 admin.site.register(PullRequest)
+admin.site.register(Moderator)
