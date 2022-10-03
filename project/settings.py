b26b63e8b869913ffadc1f09816dd7eb65f572b6
Karam <krmhmade1@gmail.com>
2022-10-03 12:28:01 +0300

    remove localhost from ALLOWED_HOSTS

--- project/settings.py
+++ project/settings.py
@@ -32,9 +32,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    "127.0.0.1",  # TODO: remove me
-]
+ALLOWED_HOSTS = []
 ALLOWED_HOSTS.append(env("HOST"))
 
 # Application definition
