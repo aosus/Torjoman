e1d8e6e157ecdf34af95cbf2774e087a229bc30c
Karam Al-Hamada <krmhmade1@gmail.com>
2023-02-07 18:57:13 +0300

    feat: add routes

--- project/api.py
+++ project/api.py
@@ -1,7 +1,19 @@
 from ninja_extra import NinjaExtraAPI
+from accounts.api import AccountsController
+from accounts.utils import AuthBearer
+from translations.api import ProjectController, SectionController, SentenceController, TranslationController
 
 api = NinjaExtraAPI(
     title="Torjoman's Core API",
     version="0.1.0",
     description="This is the Torjoman's core API",
+    auth=AuthBearer()
+)
+
+api.register_controllers(
+    ProjectController,
+    SectionController,
+    SentenceController,
+    TranslationController,
+    AccountsController,
 )
